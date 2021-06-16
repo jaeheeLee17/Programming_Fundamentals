0ac8f6ffa592ad73842b812cbe1256c54805c519
jaeheelee17 <ipod031@naver.com>
2021-06-16 12:54:37 +0900

    Initial version of Determining Program whether a year is a leap year.

---
+++ Determine_leap_year.py
@@ -0,0 +1,10 @@
+def isleapyear(year):
+    return 0 if year < 0 else ((year % 4 == 0 and year % 100 != 0) or year % 400 == 0)
+
+# test case
+for y in range(5):
+    print(y, isleapyear(y))
+for y in range(2010, 2017):
+    print(y, isleapyear(y))
+for y in range(1900, 2600, 100):
+    print(y, isleapyear(y))
